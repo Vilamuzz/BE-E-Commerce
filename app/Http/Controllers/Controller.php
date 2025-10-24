b4ca632f40921808fda59669b9812ca50b0fa79c
Vilzz <160743963+Vilamuzz@users.noreply.github.com>
2025-10-24 13:31:01 +0700

    Adding swagger to project

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,20 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *   version="1.0.0",
+ *   title="PBL E-Commerce Backend API",
+ *   description="API documentation for PBL E-Commerce backend",
+ *   @OA\Contact(name="Support", email="admin@pblecommerce.com")
+ * )
+ *
+ * @OA\Server(
+ *   url="http://localhost:8000",
+ *   description="Local development server"
+ * )
+ */
+
 abstract class Controller
 {
     //
